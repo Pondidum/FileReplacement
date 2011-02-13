c7393b9ec3f474c0b27a697cbda44885212b6d96
Pondidum <pondidum@stormbase.net>
2011-02-13 16:23:18 +0000

    new fileRepository with static methods

---
+++ FileReplacement/FileRepository2.vb
@@ -0,0 +1,19 @@
+﻿'monostate pattern
+
+Public Class FileRepository2
+
+    Private Shared _repository As FileRepositoryImpl
+
+    Shared Sub New()
+        _repository = New FileRepositoryImpl()
+    End Sub
+
+    Public Function GetFileFromDisk(ByVal path As String) As FileDescriptor
+        Return _repository.GetFileFromDisk(path)
+    End Function
+
+    Public Function GetFileFromDatabase(ByVal reader As IDataReader) As FileDescriptor
+        Return _repository.GetFileFromDatabase(reader)
+    End Function
+
+End Class
